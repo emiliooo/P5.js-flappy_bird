784ee1740ead67aef3045b4a268251226f0aaff8
Emil Buraczynski <emil.buraczynski@decsoft.com.pl>
2018-09-19 15:24:28 +0200

    add function pipes and crash with pipe

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 function Bird() {
-    this.y = width/2;
-    this.x = height/2;
+    this.y = width / 2;
+    this.x = height / 2;
 
-    this.show = function() {
+    this.gravity = 0.5;
+    this.speed = 0;
+    this.lift = -20;
+
+    this.show = function () {
         fill(255);
         ellipse(this.x, this.y, 32, 32);
-
     }
-    this.update = function() {
-        background(0);
-        bird.show();
+    this.update = function () {
+        this.speed += this.gravity
+        this.y += this.speed
+
+        if (this.y > height) {
+            this.y = height;
+            this.speed = 0;
+        } else if (this.y < 0) {
+            this.y = 0;
+              this.speed = 0;
+        } else {
+
+        }
     }
 
-
-
-
+    this.up = function () {
+        this.speed += this.lift;
+    }
 }
--- start.js
+++ start.js
@@ -1,11 +1,37 @@
 var bird;
+var pipes = [];
 
 function setup() {
-    createCanvas(400,600);
+    createCanvas(400, 600);
     bird = new Bird();
+    pipes.push(new Pipe())
 }
 
 function draw() {
     background(0);
     bird.show();
+    bird.update();
+
+    for (var i = pipes.length - 1; i >= 0; i--) {
+        pipes[i].show();
+        pipes[i].update();
+
+        if(pipes[i].hits(bird)) {
+            console.log('hit')
+        } 
+
+        if (pipes[i].offscreen()) {
+            pipes.splice(i, 1);
+        }
+    }
+
+    if (frameCount % 100 == 0) {
+        pipes.push(new Pipe())
+    }
+}
+
+function keyPressed() {
+    if (key == ' ') {
+        bird.up();
+    }
 }
